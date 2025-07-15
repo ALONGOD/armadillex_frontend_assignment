73e003b7f468489ead8758ea00e77ab5c2558c4f
Alon Goddarde <alongoddard1@gmail.com>
2025-07-15 16:14:09 +0300

    Fix double title on company list page

--- src/pages/AppIndex.vue
+++ src/pages/AppIndex.vue
@@ -1,13 +1,6 @@
 <template>
   <q-page-container>
     <div class="q-pa-md q-gutter-md">
-      <div class="row items-center justify-between q-mb-lg">
-        <div class="col">
-          <h1 class="text-h4 text-weight-bold text-primary q-mb-xs">Companies</h1>
-          <p class="text-caption text-grey-6 q-ma-none">Manage your company portfolio</p>
-        </div>
-        <div class="col-auto"></div>
-      </div>
       <router-view />
     </div>
   </q-page-container>
